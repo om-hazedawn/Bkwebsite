dbe234055136b027863963583f8fd280236299be
Om Khairnar <om.khairnar175@gmail.com>
2025-05-30 13:59:25 +0530

    added the overlays for who we are

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -13,7 +13,7 @@ export default function AboutUs() {
       case "who-we-are":
         return (
           <div>
-            <div className="relative max-w-[1690px] w-full h-[744px] mx-auto">
+            <div className="relative max-w-[1690px] w-full h-[740px] mx-auto">
               <Image
                 src="/about-us/who-we-are/BK_About_Us.png"
                 alt="Aerial view of construction site"
@@ -27,37 +27,22 @@ export default function AboutUs() {
               <div className="absolute top-0 left-0 right-0 w-full">
                 <div className="flex justify-center">
                   {/* First box - with shadow */}
-                  <div className="w-[350px] h-[372px] pl-8 pt-8 text-white transition-colors duration-300">
-                    <h3 className="text-3xl font-semibold">Our Businesses</h3>
+                  <div className="relative w-[370px] h-[370px] pl-8 pb-10 flex items-end justify-starttext-white transition-colors text-white bg-[#838182]/50">
+                    <h3 className="text-3xl font-normal">
+                      <strong>Undertake</strong> all types of construction works
+                    </h3>
                   </div>
 
                   {/* Second box - with bg-[#35b3a7] */}
-                  <Link href="/our-business/buildings">
-                    <div className="relative w-[350px] h-[372px] pl-8 pb-10 flex items-end justify-start bg-[#1aabaf]/80 text-white transition-colors duration-300 hover:bg-[#1aabaf]/40">
-                      <h3 className="text-3xl font-semibold">Buildings</h3>
-                      <div className="absolute top-4 right-5 w-8 h-8">
-                        <Image
-                          src="/our-business/white_co.webp"
-                          alt="Corner decoration"
-                          fill
-                          className="object-contain"
-                        />
-                      </div>
-                    </div>
-                  </Link>
+
+                  <div className="relative w-[370px] h-[370px]"></div>
 
                   {/* Third box - with bg-[#e63946] */}
                   <Link href="/our-business/environmental">
-                    <div className="relative w-[350px] h-[372px] pl-8 pb-10 flex items-end justify-start bg-[#e63946]/80 text-white transition-colors duration-300 hover:bg-[#e63946]/40">
-                      <h3 className="text-3xl font-semibold">Environmental</h3>
-                      <div className="absolute top-4 right-5 w-8 h-8">
-                        <Image
-                          src="/our-business/white_co.webp"
-                          alt="Corner decoration"
-                          fill
-                          className="object-contain"
-                        />
-                      </div>
+                    <div className="relative w-[370px] h-[370px] pl-8 pb-10 flex items-end justify-start bg-[#E63946D6] text-white">
+                      <h3 className="text-3xl font-normal">
+                        We provide <strong>full project life-cycle</strong> services
+                      </h3>
                     </div>
                   </Link>
                 </div>
@@ -67,54 +52,19 @@ export default function AboutUs() {
               <div className="absolute top-[372px] left-0 right-0 w-full">
                 <div className="flex justify-center">
                   {/* First box - with shadow */}
-                  <Link href="/our-business/foundations">
-                    <div className="relative w-[350px] h-[372px] pl-8 pb-10 flex items-end justify-start  bg-[#D5D4D5] text-black transition-colors duration-300 hover:bg-[#D5D4D5]/40">
-                      <h3 className="text-3xl font-semibold">Foundations</h3>
-                      <div className="absolute top-4 right-5 w-8 h-8">
-                        <Image
-                          src="/our-business/gray_co.webp"
-                          alt="Corner decoration"
-                          fill
-                          className="object-contain"
-                        />
-                      </div>
-                    </div>
-                  </Link>
+                  <div className="relative w-[370px] h-[370px]"></div>
 
                   {/* Second box - with bg-[#35b3a7] */}
                   <Link href="/our-business/civil-works">
-                    <div className="relative w-[350px] h-[372px] pl-8 pb-10 flex items-end justify-start text-white transition-all duration-300 shadow-lg hover:bg-black/40 hover:shadow-none">
-                      <h3 className="text-3xl font-semibold">
-                        General Civil Works
+                    <div className="relative w-[370px] h-[370px] pl-8 pb-10 flex items-end justify-start text-white bg-[#35B3A7D6]">
+                      <h3 className="text-3xl font-normal">
+                        We pursue <strong>teamwork</strong> and <strong>trust</strong>
                       </h3>
-                      <div className="absolute top-4 right-5 w-8 h-8">
-                        <Image
-                          src="/our-business/white_co.webp"
-                          alt="Corner decoration"
-                          fill
-                          className="object-contain"
-                        />
-                      </div>
                     </div>
                   </Link>
 
                   {/* Third box - with bg-[#e63946] */}
-                  <Link href="/our-business/railway-works">
-                    <div className="relative w-[350px] h-[372px] pl-8 pb-10 flex items-end justify-start bg-[#1aabaf]/80 text-white transition-colors duration-300 hover:bg-[#1aabaf]/40">
-                      <h3 className="text-3xl font-semibold">
-                        Railway &<br />
-                        Associated Works
-                      </h3>
-                      <div className="absolute top-4 right-5 w-8 h-8">
-                        <Image
-                          src="/our-business/white_co.webp"
-                          alt="Corner decoration"
-                          fill
-                          className="object-contain"
-                        />
-                      </div>
-                    </div>
-                  </Link>
+                  <div className="relative w-[370px] h-[370px]"></div>
                 </div>
               </div>
             </div>
@@ -127,26 +77,45 @@ export default function AboutUs() {
               />
               <div className="absolute inset-0 flex items-center justify-center bg-black/20">
                 <p className="text-white text-4xl w-[1300px] font-normal">
-                  <strong>Build King</strong> has developed a unique competitive edge - a lean
-                  management adopting an entrepreneurial spirit that enable
-                  quick decision making and rapid responses for result oriented.
+                  <strong>Build King</strong> has developed a unique competitive
+                  edge - a lean management adopting an entrepreneurial spirit
+                  that enable quick decision making and rapid responses for
+                  result oriented.
                 </p>
               </div>
             </div>
 
             {/* Who We Are Section */}
             <div className="mt-16 max-w-[1300px] mx-auto">
-              <h2 className="text-4xl font-bold mb-6 text-[#838182]">Who We Are</h2>
+              <h2 className="text-4xl font-bold mb-6 text-[#838182]">
+                Who We Are
+              </h2>
               <h3 className="text-3xl mb-6 text-[#838182]">Our Mission</h3>
               <div className="text-[#838182] space-y-6">
                 <p>
-                  Build King focuses on becoming one of the top construction companies in Hong Kong and the preferred partner for customers, subcontractors, suppliers and joint ventures. To achieve these goals, we have developed a unique competitive edge - a lean management with an entrepreneurial style that enable quick decision making and rapid responses. Our highly motivated management team includes more than 300 professional engineers and commercial specialists and is supported by over 3000 other staff;
+                  Build King focuses on becoming one of the top construction
+                  companies in Hong Kong and the preferred partner for
+                  customers, subcontractors, suppliers and joint ventures. To
+                  achieve these goals, we have developed a unique competitive
+                  edge - a lean management with an entrepreneurial style that
+                  enable quick decision making and rapid responses. Our highly
+                  motivated management team includes more than 300 professional
+                  engineers and commercial specialists and is supported by over
+                  3000 other staff;
                 </p>
                 <p>
-                  We offer comprehensive construction service in 3 major areas - civil engineering, buildings and environmental. With a proactive management philosophy and multidisciplinary competency, we bring broad-based skills to bear on great variety of projects in both the private and public sectors.
+                  We offer comprehensive construction service in 3 major areas -
+                  civil engineering, buildings and environmental. With a
+                  proactive management philosophy and multidisciplinary
+                  competency, we bring broad-based skills to bear on great
+                  variety of projects in both the private and public sectors.
                 </p>
                 <p>
-                  Our services cover full project life-cycle including: design, construction, testing and commissioning, maintenance, facility management and operation. We undertake all types of construction works with contract sums in excess of billions of Hong Kong dollars.
+                  Our services cover full project life-cycle including: design,
+                  construction, testing and commissioning, maintenance, facility
+                  management and operation. We undertake all types of
+                  construction works with contract sums in excess of billions of
+                  Hong Kong dollars.
                 </p>
               </div>
             </div>
@@ -240,7 +209,7 @@ export default function AboutUs() {
             />
           </div>
         </div>
-        <div className="container mx-auto">{renderContent()}</div>
+        <div className="container mx-auto mb-20">{renderContent()}</div>
       </section>
       <Footer />
     </main>
